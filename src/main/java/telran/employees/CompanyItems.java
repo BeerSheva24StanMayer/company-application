217309e49fab6a9bd53a436b19b4bbb653447aa8
Stan Mayer <sthumor@gmail.com>
2024-10-21 13:49:03 +0300

    CW36

---
+++ src/main/java/telran/employees/CompanyItems.java
@@ -0,0 +1,13 @@
+package telran.employees;
+
+import telran.view.*;
+
+public class CompanyItems {
+    //TODO
+    public static Item[] getItems(Company company) {
+        //TODO
+        //item for each service method
+        return null;
+    }
+
+}
